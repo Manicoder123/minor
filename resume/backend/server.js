42e279d021ffb9ca1403dc5748c44ab70730fc02
Manicoder123 <edulamanibhaskar@gmail.com>
2023-03-10 21:23:14 +0530

    second commit

---
+++ resume/backend/server.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const User=require('./Schemas/userSchema');
+
+const app = express();
+const port = process.env.PORT || 3001;
+const dbURI = process.env.DB_URI;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
+
+
+app.get('/users',async(req,res)=>{
+    const users=await User.find();
+    res.json(users);
+});
+
+app.post('/users/new',async(req,res)=>{
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password:req.body.password,
+      branch:req.body.branch,
+      regno:req.body.regno,
+      });
+      
+newUser.save()
+    .then(() => {
+      console.log('User saved to database');
+      res.json('User saved to database');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json('Error saving user to database');
+    });
+});
---
+++ resume/src/Home/AdminLogin/Admin/Admin.js
@@ -0,0 +1,105 @@
+import React,{useState} from 'react';
+import Axios from 'axios';
+
+const Admin = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        password: "",
+        email:"",
+        branch:"",
+        regno:"",
+      });
+
+      const validateEmail = (email) => {
+        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        return regex.test(email);
+      }
+
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if(validateEmail(formData.email) && formData.name!=="" && formData.password!=="" && formData.email!=="" && formData.branch!=="" && formData.regno!==""){
+            Axios.post('http://localhost:3001/users/new',formData)
+                .then((response) => {
+                console.log('Response:', response.data);
+                })
+                .catch((error) => {
+                console.log('Error:', error.message);
+                });
+            window.location.reload();
+            window.alert("User saved");
+        }else{
+            window.alert("You may have typed the wrong email or unfilled the details");
+        }
+      };
+
+      const handleChange = (e) => {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value,
+        });
+      };
+
+    return (
+        <div>
+        <form onSubmit={handleSubmit}>
+  <div>
+    <label htmlFor="name">name:</label>
+    <input
+      type="text"
+      name="name"
+      value={formData.name}
+      maxLength={20}
+      onChange={handleChange}
+    />
+  </div>
+  <div>
+    <label htmlFor="password">Password:</label>
+    <input
+      type="password"
+      name="password"
+      value={formData.password}
+      onChange={handleChange}
+      maxLength={20}
+    />
+  </div>
+  <div>
+    <label htmlFor="email">email:</label>
+    <input
+      type="string"
+      name="email"
+      value={formData.email}
+      onChange={handleChange}
+    />
+  </div>
+  <div>
+    <label htmlFor="branch">branch:</label>
+    <input
+      type="string"
+      name="branch"
+      value={formData.branch}
+      onChange={handleChange}
+      maxLength={10}
+    />
+    <div>
+    <label htmlFor="regno">regno:</label>
+    <input
+      type="string"
+      name="regno"
+      value={formData.regno}
+      onChange={handleChange}
+      maxLength={15}
+    />
+  </div>
+  </div>
+  <button type="submit">create</button>
+</form>
+
+
+    </div>
+    );
+}
+
+export default Admin;
